c049de7d8c9280129f634c466bd2ce8c237a10bd
maheshkumarsanika <sanikamaheshkumar@gmail.com>
2022-11-07 13:56:23 +0530

    shopping list page with api integrated

---
+++ src/pages/shopping-list.page.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import React, { Component } from "react";
+import Header from "../components/header/header.component";
+import Product from "../components/product/product.component";
+import Search from "../components/search/searchbar.component.";
+
+export default class ShoppingListPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://fakestoreapi.com/products").then((res) => {
+      this.setState({
+        products: res.data,
+      });
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <>
+        <div className="header">
+          <Header title="Shopping Cart" subTitle="Shop unlimited products" />
+        </div>
+        <div className="search mt-6 is small">
+          <Search />
+        </div>
+        <h2 className="head ml-6 has-text-info has-text-weight-bold is-size-3">
+          Products
+        </h2>
+        <div className="columns is-multiline px-6 py-3">
+          {products.map((product) => (
+            <Product key={product.id} data={product} />
+          ))}
+        </div>
+      </>
+    );
+  }
+}
