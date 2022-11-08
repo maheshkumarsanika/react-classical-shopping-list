f3084657f2273e99263bd95b2606c16746d6eadc
maheshkumarsanika <sanikamaheshkumar@gmail.com>
2022-11-08 12:45:17 +0530

    header component with dynamic active link logic

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,24 +1,76 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Header extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.navLinks = [
+      {
+        name: "Home",
+        path: "/",
+      },
+      {
+        name: "Products",
+        path: "/products",
+      },
+      {
+        name: "Categories",
+        path: "/categories?search",
+      },
+      {
+        name: "Date Range",
+        path: "/date-range",
+      },
+    ];
   }
 
+  getActiveLinkBackgroundColor = (path) => {
+    const routeName = window.location.pathname;
+    if (routeName === path) {
+      return "grey";
+    }
+
+    if (routeName !== "/" && path.includes(routeName)) {
+      return "grey";
+    }
+
+    return "";
+  };
+
   render() {
-    const { title, subTitle } = this.props;
+    const { title } = this.props;
 
     return (
-      <div className="is-primary has-text-primary">
-        <section className="hero is-link">
-          <div className="hero-body">
-            <p className="title">{title}</p>
-            <p className="subtitle">{subTitle}</p>
+      <nav
+        className="navbar is-info"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <h2 className="title is-4 p-3 has-text-white">{title}</h2>
+        </div>
+        <div
+          id="navbarBasicExample"
+          className="navbar-menu"
+          style={{ marginLeft: "900px" }}
+        >
+          <div className="navbar-start">
+            {this.navLinks.map((navLink) => (
+              <Link
+                className="navbar-item ml-1"
+                key={navLink.name}
+                to={navLink.path}
+                style={{
+                  background: this.getActiveLinkBackgroundColor(navLink.path),
+                }}
+              >
+                {navLink.name}
+              </Link>
+            ))}
           </div>
-        </section>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
