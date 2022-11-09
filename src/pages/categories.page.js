6be3647b03fdc72896d5334465187bdbc4e7afda
maheshkumarsanika <sanikamaheshkumar@gmail.com>
2022-11-09 08:13:36 +0530

    Category page task

--- src/pages/categories.page.js
+++ src/pages/categories.page.js
@@ -3,25 +3,83 @@ import React, { Component } from "react";
 import Header from "../components/header/header.component";
 import Product from "../components/product/product.component";
 
+const mockMultiCategories = async () => {
+  const categories = [
+    {
+      name: "electronics",
+    },
+    {
+      name: "jewelery",
+    },
+    {
+      name: "clothing",
+      sub_categories: [
+        {
+          name: "men's clothing",
+          sub_categories: [
+            {
+              name: "shirts",
+            },
+          ],
+        },
+        {
+          name: "women's clothing",
+        },
+      ],
+    },
+  ];
+
+  return new Promise((res, rej) => res(categories));
+};
+
 export default class Categories extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      categories: [],
       products: [],
+      category: "",
     };
   }
-
-  componentDidMount() {
-    axios.get("https://fakestoreapi.com/products").then((res) => {
+  handleClick(c) {
+    axios.get("https://fakestoreapi.com/products/category/" + c).then((res) => {
       this.setState({
         products: res.data,
+        category: c,
       });
     });
   }
+  componentDidMount() {
+    // axios.get("https://fakestoreapi.com/products/categories").then((res) => {
+    mockMultiCategories().then((res) => {
+      this.setState(
+        {
+          categories: res,
+        },
+        () => {
+          this.handleClick(this.state.categories[0].name);
+        }
+      );
+    });
+  }
+
+  renderCatergories = (categories) => {
+    return categories.map((c) => (
+      <div
+        className="py-2 pl-3 m-2 has-background-grey has-text-white is-capitalized"
+        key={c.name}
+        onClick={() => !c.sub_categories && this.handleClick(c.name)}
+      >
+        {c.name}
+        {c.sub_categories && this.renderCatergories(c.sub_categories)}
+      </div>
+    ));
+  };
 
   render() {
-    const { products } = this.state;
+    const { categories, products, category } = this.state;
+
     return (
       <>
         <div className="header">
@@ -31,9 +89,21 @@ export default class Categories extends Component {
           Categories
         </h2>
         <div className="columns is-multiline px-6 py-3">
-          {products.map((product) => (
-            <Product key={product.id} data={product} />
-          ))}
+          <div className="column is-2 pt-6">
+            {this.renderCatergories(categories)}
+          </div>
+          <div className="column is-10">
+            <h2 className="head m-6 has-text-info has-text-weight-bold is-size-3 is-capitalized">
+              {products.length
+                ? `${category} Products`
+                : `No Products in ${category}`}
+            </h2>
+            <div className="columns is-multiline">
+              {products.map((product) => (
+                <Product key={product.id} data={product} />
+              ))}
+            </div>
+          </div>
         </div>
       </>
     );
